5375cedf2e82d3b575b1865161852a2fd96fbd6f
thanabordee <jejekup00123@hotmail.co.th>
2023-02-21 16:46:03 +0700

    complete

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+	double  x,y,w,h;
 };
 
-double overlap(){
-
-
-
-}
+double overlap(Rect *R1, Rect *R2){
+  double area;
+  
+	if ((min(R1->x+R1->w,R2->x+R2->w)-max(R1->x,R2->x)) <=  0 || (min(R1->y+R1->h,R2->y+R2->h)-max(R1->y,R2->y)) <=0)
+	{
+		return 0;
+	}
+	else{
+	area = (min((R1->x+R1->w),R2->x+R2->w)-max(R1->x,R2->x))*(min(R1->y,R2->y)-max(R1->y-R1->h,R2->y-R2->h));
+    return area;
+	}
+   }
--- lab20_3.cpp
+++ lab20_3.cpp
@@ -10,7 +10,10 @@ using namespace std;
 struct student{
 
     //Define struct student with four member (name ,id , gender, gpa);
-    
+    char name[100];
+	int id;
+	char gender[10];
+	float gpa;
 };
 
 struct course{
@@ -20,7 +23,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -63,6 +66,11 @@ int main(){
 	
 	while(getline(student_file,textline)){
 		student s; 
+		char format[] = "%[^,],%d,%[^,],%f";
+		
+		sscanf(textline.c_str(),format,s.name,&s.id,s.gender,&s.gpa);
+     
+
 		
 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
 
@@ -83,9 +91,9 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
+			 
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
+			    allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
@@ -93,6 +101,7 @@ int main(){
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
 				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
